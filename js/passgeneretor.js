ba3824a1802cfa7981153b5de0b56cf621f1807e
luigi2001 <luigitanga@virgilio.it>
2021-05-26 16:48:55 +0200

    add script

--- js/passgeneretor.js
+++ js/passgeneretor.js
@@ -1,1 +1,11 @@
-alert ('ciao');
+
+var nome = prompt ('inserisci il tuo nome');
+console.log (nome);
+
+var cognome = prompt ('insersci il tuo cognome');
+console.log (cognome);
+
+var colorePreferito = prompt ('colore preferito');
+console.log (colorePreferito);
+
+alert ('la tua password è:'+ nome + cognome + colorePreferito + '21');
